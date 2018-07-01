8f6eeba083e74e23178d4c5c502e144feb0a0fee
Eric <eric@Erics-MacBook-Air.local>
2018-07-01 15:41:01 -0700

    Troubleshooting

--- lib/rest/RouteAuthenticator.ts
+++ lib/rest/RouteAuthenticator.ts
@@ -3,6 +3,7 @@ import { Request, Response, NextFunction } from "express";
 
 export default class RouteAuthenticator{
     public isAuthenticated(req: Request, res: Response, next: NextFunction){
+        console.log("req.session.subject: ", req.session.subject);
         if(req.session.subject){
             next();
         }else{
